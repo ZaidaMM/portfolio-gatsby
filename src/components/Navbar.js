cf0d91ba398e06f31ca318eff9fc8e9fb000b401
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-11-18 10:04:35 +0000

    navbar setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,32 @@
 import React from "react"
+import { FaAlignRight } from "react-icons/fa"
+import { Link } from "gatsby"
+import pageLinks from "../constants/links"
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <h2 className="logo">
+            <span>Web</span>
+            Dev
+          </h2>
+          <button className="toggle-btn" type="button">
+            <FaAlignRight />
+          </button>
+        </div>
+        <div className="nav-links">
+          {pageLinks.map(link => {
+            return (
+              <Link key={link.id} to={link.url}>
+                {link.text}
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+    </nav>
+  )
 }
 export default Navbar

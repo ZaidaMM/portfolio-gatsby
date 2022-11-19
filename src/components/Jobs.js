ea7ab780219be8308643a1aff15ba15e30945ca1
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-11-19 07:07:18 +0000

    jobs page setup

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,6 +1,27 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import Title from "./Title"
+
+const query = graphql`
+  query {
+    allStrapiJob {
+      nodes {
+        position
+        company
+        date
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`
 
 const Jobs = () => {
+  const data = useStaticQuery(query)
+  const jobs = data.allStrapiJob.nodes
+  console.log(data)
   return <div>Jobs</div>
 }
 export default Jobs
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       <main>
         <Hero />
         <Services />
+        <Jobs />
       </main>
     </>
   )

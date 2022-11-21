ec415af6ce85ce706ff2ccff43f497503744499d
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-11-21 14:03:47 +0000

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,23 @@
 import React from "react"
+import socialLinks from "../constants/social_links"
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className="footer">
+      <div className="footer-links social-links">
+        {socialLinks.map(link => {
+          return (
+            <a href={link.url} key={link.id} className="social-link">
+              {link.icon}
+            </a>
+          )
+        })}
+      </div>
+      <h5>
+        Copyright &copy; {new Date().getFullYear()} <span>Zaida Montano</span>
+        All rights reserved
+      </h5>
+    </footer>
+  )
 }
 export default Footer

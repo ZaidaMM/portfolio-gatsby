7c40d8a3fe749c5bc443b6be3c49a233b07275e5
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-11-21 12:14:45 +0000

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,52 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Title from "../components/Title"
 
-const about = () => {
-  return <div>about</div>
+const about = ({ data }) => {
+  const {
+    strapiAbout: { title, info, stack, image },
+  } = data
+  console.log(data)
+  return (
+    <>
+      <section className="about-page">
+        <div className="section-center about-center">
+          <img
+            src={image[0].localFile.publicURL}
+            alt={title}
+            className="about-img-svg"
+          />
+          <article className="about-text">
+            <Title title={title} />
+            <p>{info}</p>
+            <div className="about-stack">
+              {stack.map(item => {
+                return <span key={item.id}>{item.title}</span>
+              })}
+            </div>
+          </article>
+        </div>
+      </section>
+    </>
+  )
 }
+
+export const query = graphql`
+  {
+    strapiAbout {
+      title
+      info
+      stack {
+        id
+        title
+      }
+      image {
+        localFile {
+          publicURL
+        }
+      }
+    }
+  }
+`
+
 export default about

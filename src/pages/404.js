40ffcb2175a5daa24bf30020d3291f53ed5094c4
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-11-21 12:21:01 +0000

    error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const error = () => {
-  return <div>404</div>
+  return (
+    <main className="error-page">
+      <div className="error-container">
+        <h1>page not found</h1>
+        <Link to="/" className="btn">
+          Back Home
+        </Link>
+      </div>
+    </main>
+  )
 }
 export default error

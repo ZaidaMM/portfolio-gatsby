46fcc5821d460b0634d1f1e73679d18b1cd32053
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-11-22 06:44:24 +0000

    mobile sidebar setup

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,43 @@
 import React from "react"
+import pageLinks from "../constants/links"
+import socialLinks from "../constants/social_links"
+import { Link } from "gatsby"
+import { FaTimes } from "react-icons/fa"
 
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  const isOpen = true
+  return (
+    <>
+      <aside className={isOpen ? "sidebar show-sidebar" : "sidebar"}>
+        <button type="button" className="close-btn">
+          <FaTimes />
+        </button>
+        <div className="side-container">
+          <ul className={isOpen ? "sidebar-links" : null}>
+            {pageLinks.map(link => {
+              return (
+                <li key={link.id}>
+                  <Link to={link.url} className="sidebar-links">
+                    {link.text}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+          <ul className={isOpen ? "social-links sidebar-icons" : null}>
+            {socialLinks.map(link => {
+              return (
+                <li key={link.id}>
+                  <a href={link.url} className="social-link">
+                    {link.icon}
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </aside>
+    </>
+  )
 }
 export default Sidebar
